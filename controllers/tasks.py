4845ac5700d88259d00d5ac40628d51be17762c8
Khanh Luong <khanhluong3005@gmail.com>
2021-09-01 10:29:09 +0700

    Merge *_task and *_runner together

--- controllers/tasks.py
+++ controllers/tasks.py
@@ -6,52 +6,52 @@ from controllers import bitvise_controllers
 from models import ports, ssh_models
 
 
-async def _ssh_check_runner():
+async def ssh_check_task(is_runner=False):
     """
     Check and update SSH status one by one
+    :param is_runner: True if this function call is to spawn child runners.
+    False means this is main call.
     """
-    while True:
-        # Reduce system load
-        await asyncio.sleep(5)
+    if not is_runner:
+        await asyncio.gather(
+            *[ssh_check_task(is_runner=True) for _ in range(20)]
+        )
+    else:
+        while True:
+            # Reduce system load
+            await asyncio.sleep(5)
 
-        next_ssh = ssh_models.get_ssh_to_check()
-        if not next_ssh:
-            continue
+            next_ssh = ssh_models.get_ssh_to_check()
+            if not next_ssh:
+                continue
 
-        is_live = await bitvise_controllers.verify_ssh(next_ssh.ip,
-                                                       next_ssh.username,
-                                                       next_ssh.password)
-        ssh_models.update_ssh_status(next_ssh, is_live=is_live)
-
-
-async def ssh_check_task():
-    """
-    Wrapper for concurrent run of _ssh_check_runner()
-    """
-    await asyncio.gather(*[_ssh_check_runner() for _ in range(20)])
+            is_live = await bitvise_controllers.verify_ssh(next_ssh.ip,
+                                                           next_ssh.username,
+                                                           next_ssh.password)
+            ssh_models.update_ssh_status(next_ssh, is_live=is_live)
 
 
-async def _port_check_runner():
+async def port_check_task(is_runner=False):
     """
     Check and update port status (can proxy through or not) one by one
+    :param is_runner: True if this function call is to spawn child runners.
+    False means this is main call.
     """
-    while True:
-        # Reduce system load
-        await asyncio.sleep(5)
+    if not is_runner:
+        await asyncio.gather(
+            *[port_check_task(is_runner=True) for _ in range(20)]
+        )
+    else:
+        while True:
+            # Reduce system load
+            await asyncio.sleep(5)
 
-        next_port = ports.get_port_to_check()
-        if not next_port:
-            continue
+            next_port = ports.get_port_to_check()
+            if not next_port:
+                continue
 
-        ip = await bitvise_controllers.get_proxy_ip(next_port.proxy_address)
-        ports.update_port_ip(next_port, ip=ip)
-
-
-async def port_check_task():
-    """
-    Wrapper for concurrent run of _port_check_runner()
-    """
-    await asyncio.gather(*[_port_check_runner() for _ in range(20)])
+            ip = await bitvise_controllers.get_proxy_ip(next_port.proxy_address)
+            ports.update_port_ip(next_port, ip=ip)
 
 
 def reset_ssh_and_port_status():
