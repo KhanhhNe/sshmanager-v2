748799991b7c37fdffe5eb1d8e5435e3eee32241
Khanh Luong <khanhluong3005@gmail.com>
2021-09-01 15:57:20 +0700

    Move is_runner in tasks to runners() wrapper

--- tasks.py
+++ tasks.py
@@ -1,11 +1,20 @@
 import asyncio
-from typing import List, Type, Union
+from functools import wraps
+from typing import List, Type, Union, cast
 
 import psutil
 from pony.orm import db_session
 
 from controllers import bitvise_controllers
 from models import port_models, ssh_models
+
+
+def runners(func):
+    @wraps
+    async def wrapped(*args, **kwargs):
+        await asyncio.gather(*[func(*args, **kwargs) for _ in range(20)])
+
+    return cast(func, wrapped)
 
 
 @db_session
@@ -27,60 +36,48 @@ def get_check_next(cls: Union[Type[SSH], Type[Port]]):
         return None
 
 
-async def ssh_check_task(is_runner=False):
+@runners
+async def ssh_check_task():
     """
     Check and update SSH status one by one
-    :param is_runner: True if this function call is to spawn child runners.
-    False means this is main call.
     """
-    if not is_runner:
-        await asyncio.gather(
-            *[ssh_check_task(is_runner=True) for _ in range(20)]
-        )
-    else:
-        while True:
-            # Reduce system load
-            await asyncio.sleep(5)
+    while True:
+        # Reduce system load
+        await asyncio.sleep(5)
 
-            next_ssh = get_check_next(ssh_models.SSH)
-            if not next_ssh:
-                continue
+        next_ssh = get_check_next(ssh_models.SSH)
+        if not next_ssh:
+            continue
 
-            is_live = await bitvise_controllers.verify_ssh(next_ssh.ip,
-                                                           next_ssh.username,
-                                                           next_ssh.password)
-            with db_session:
-                ssh: SSH = ssh_models.SSH[next_ssh.id]
-                if ssh:
-                    ssh.last_checked = datetime.now()
-                    ssh.is_live = is_live
+        is_live = await bitvise_controllers.verify_ssh(next_ssh.ip,
+                                                       next_ssh.username,
+                                                       next_ssh.password)
+        with db_session:
+            ssh: SSH = ssh_models.SSH[next_ssh.id]
+            if ssh:
+                ssh.last_checked = datetime.now()
+                ssh.is_live = is_live
 
 
-async def port_check_task(is_runner=False):
+@runners
+async def port_check_task():
     """
     Check and update port status (can proxy through or not) one by one
-    :param is_runner: True if this function call is to spawn child runners.
-    False means this is main call.
     """
-    if not is_runner:
-        await asyncio.gather(
-            *[port_check_task(is_runner=True) for _ in range(20)]
-        )
-    else:
-        while True:
-            # Reduce system load
-            await asyncio.sleep(5)
+    while True:
+        # Reduce system load
+        await asyncio.sleep(5)
 
-            next_port = get_check_next(port_models.Port)
-            if not next_port:
-                continue
+        next_port = get_check_next(port_models.Port)
+        if not next_port:
+            continue
 
-            ip = await bitvise_controllers.get_proxy_ip(next_port.proxy_address)
-            with db_session:
-                port: Port = port_models.Port[next_port.id]
-                if port:
-                    port.last_checked = datetime.now()
-                    port.ip = ip
+        ip = await bitvise_controllers.get_proxy_ip(next_port.proxy_address)
+        with db_session:
+            port: Port = port_models.Port[next_port.id]
+            if port:
+                port.last_checked = datetime.now()
+                port.ip = ip
 
 
 def reset_ssh_and_port_status():
