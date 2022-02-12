5e3a05a61dbd181e2928b67ca61dc9c11647cdda
Khanh Luong <khanhluong3005@gmail.com>
2022-02-12 18:56:43 +0700

    Add kill_process_on_port() utils
    New util added to kill all child processes running on given port.

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import asyncio
 import socket
 
+import psutil
+
 import config
 from models.database import db
 
@@ -28,17 +30,17 @@ def can_connect_to_socket(host, port):
     """
     Try to connect to host:port using socket. Returns whether the connection
     succeed or not.
+
     :param host: Target host
     :param port: Target port
     :return: True if connected successfully. False otherwise.
     """
-    conf = config.get_config()
-    try:
-        connection_timeout = conf.getint('SSH', 'connection_timeout')
-        socket.create_connection((host, port), connection_timeout)
-        return True
-    except (ConnectionError, TimeoutError, socket.timeout):
-        return False
+    timeout = config.get_config().getint('SSH', 'connection_timeout')
+    with socket.socket() as s:
+        s.settimeout(timeout)
+        if s.connect_ex((host, port)):
+            return False
+    return True
 
 
 async def wait_for_db_update():
@@ -49,3 +51,41 @@ async def wait_for_db_update():
         if not db.last_sql.startswith('SELECT'):
             return
         await asyncio.sleep(1)
+
+
+async def kill_process_on_port(port_number: int):
+    """
+    Kill all child processes running on given port.
+
+    :param port_number: Target port number
+    :return: Whether any child process running on `port_number` is found
+    """
+
+    async def run_shell_command(command):
+        completed = await asyncio.create_subprocess_shell(
+            command,
+            stdout=asyncio.subprocess.PIPE)
+        return await completed.stdout.read()
+
+    netstat = await run_shell_command("netstat -ano")
+    pids = []
+
+    # Get all process pids
+    for line in netstat.splitlines():
+        _, local_address, *_, pid = line.split()
+        if local_address.endswith(f":{port_number}"):
+            pids.append(int(pid))
+
+    child_pids = [p.pid for p in psutil.Process().children(recursive=True)]
+
+    if pids:
+        # Kill the processes
+        loop = asyncio.get_running_loop()
+        for pid in pids:
+            if pid in child_pids:
+                await loop.run_in_executor(
+                    None, lambda i: psutil.Process(i).kill(), pid
+                )
+        return True
+    else:
+        return False
