bf6ba7039a15e08c39f3b5da4381aa1e84713cba
Khanh Luong <khanhluong3005@gmail.com>
2022-05-27 20:10:05 +0700

    Add Pinia to app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import {createApp, h} from 'vue'
 import App from './App.vue'
 import moment from "moment";
+import {createPinia} from "pinia";
 
 moment.locale('vi')
 
 const app = createApp({
     render: () => h(App),
 })
+app.use(createPinia())
 app.mount('#app')
