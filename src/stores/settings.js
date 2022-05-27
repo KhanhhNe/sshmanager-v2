220ea4613297768e14af0fc29d4134a848ea7748
Khanh Luong <khanhluong3005@gmail.com>
2022-05-27 20:05:33 +0700

    Add Settings store

---
+++ src/stores/settings.js
@@ -0,0 +1,50 @@
+import {defineStore} from 'pinia'
+import _ from "lodash";
+
+export const useSettingsStore = defineStore('settings', {
+    state: () => ({
+        settings: {},
+        originalSettings: "{}"
+    }),
+    getters: {
+        isChanged(state) {
+            return !_.isEqual(JSON.parse(state.originalSettings), state.settings)
+        }
+    },
+    actions: {
+        async loadSettings() {
+            const settingsValues = await (await fetch('/api/settings')).json()
+            const settingsNames = await (await fetch('/api/settings/names')).json()
+            const settings = {}
+            for (const [name, value] of Object.entries(settingsValues)) {
+                settings[name] = {value, readable_name: settingsNames[name]}
+            }
+            this.settings = settings
+            this.originalSettings = JSON.stringify(settings)
+        },
+
+        async revertSettings() {
+            await this.loadSettings()
+        },
+
+        async updateSettings() {
+            const settings = {}
+            for (const [name, setting] of Object.entries(this.settings)) {
+                settings[name] = setting.value
+            }
+            await fetch('/api/settings', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(settings)
+            })
+            await this.loadSettings()
+        },
+
+        async resetSettings() {
+            await fetch('/api/settings', {method: 'delete'})
+            await this.loadSettings()
+        }
+    }
+})
