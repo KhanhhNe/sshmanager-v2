7ed4c98147252cf6e92ce94bf79a5de3a7ae5ba8
Khanh Luong <khanhluong3005@gmail.com>
2022-05-28 10:17:47 +0700

    Support multiple web workers

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import sys
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from models.database import generate_mappings
 from views import ports_api, settings_api, ssh_api
 
 os.environ['PATH'] += ';executables'
@@ -13,6 +14,12 @@ package_data = json.load(open('package.json', encoding='utf-8'))
 app = FastAPI(title="SSHManager by KhanhhNe",
               description=package_data['description'],
               version=package_data['version'])
+
+
+@app.on_event('startup')
+def startup_event():
+    generate_mappings()
+
 
 # Routers
 app.include_router(ssh_api.router, prefix='/api/ssh')
