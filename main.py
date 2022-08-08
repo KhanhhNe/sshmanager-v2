f25752c945658e2640829db193ea4bcbfcff8e7e
Khanh Luong <khanhluong3005@gmail.com>
2022-08-08 18:55:50 +0700

    Use psutil child process to kill other child processes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import atexit
 import functools
 import logging
 import multiprocessing
@@ -8,6 +9,7 @@ from traceback import format_exc
 
 import cryptography
 import hypercorn.trio
+import psutil
 import trio
 import trio_asyncio
 from hypercorn.run import run
@@ -24,6 +26,7 @@ from controllers import tasks, actions
 from models import init_db
 
 logger = logging.getLogger('Main')
+exited = multiprocessing.Event()
 
 
 async def run_tasks():
@@ -34,23 +37,33 @@ async def run_tasks():
         nursery.start_soon(tasks.run_all_tasks)
 
 
-def run_hypercorn_server(conf: hypercorn.Config):
+def run_hypercorn_server(hypercorn_conf: hypercorn.Config):
     utils.configure_logging()
-    run(conf)
+    run(hypercorn_conf)
+
+
+def kill_all_processes(parent_pid: int):
+    try:
+        exited.wait()
+    except KeyboardInterrupt:
+        pass
+
+    children = psutil.Process(pid=parent_pid).children(recursive=True)
+    children = [p for p in children if p.pid != os.getpid()]
+    print(children)
+
+    for child in children:
+        child.terminate()
+    gone, alive = psutil.wait_procs(children, timeout=1)
+    print(gone, alive)
+    for child in alive:
+        child.kill()
 
 
 if __name__ == '__main__':
     # freeze_support()
     os.makedirs('data', exist_ok=True)
     port = config.get('web_port')
-
-    if os.environ.get("DEBUG"):
-        # Remove config.ini file by default when debugging
-        if os.path.exists('data/config.ini'):
-            os.remove('data/config.ini')
-    else:
-        # Open the web browser pointing to app's URL
-        webbrowser.open_new_tab(f"http://{utils.get_ipv4_address()}:{port}")
 
     # Logging related config
     utils.configure_logging()
@@ -62,13 +75,28 @@ if __name__ == '__main__':
     conf.accesslog = '-'
     conf.errorlog = '-'
     conf.worker_class = 'trio'
-    conf.workers = 1
     conf.application_path = 'app:app'
-    conf.use_reloader = True
+
+    if os.environ.get("DEBUG"):
+        # Reset config.ini to default values
+        if os.path.exists('data/config.ini'):
+            os.remove('data/config.ini')
+
+        # Use reloader for convenient development
+        conf.use_reloader = True
+    else:
+        # Open the web browser pointing to app's URL
+        webbrowser.open_new_tab(f"http://{utils.get_ipv4_address()}:{port}")
+
+        # Use multiple worker to speed up the server
+        conf.workers = multiprocessing.cpu_count()
+
+    # Make sure to set the exit event after this process exits
+    atexit.register(exited.set)
 
     run_server = functools.partial(run_hypercorn_server, conf)
-    process = multiprocessing.Process(target=run_server)
-    process.start()
+    multiprocessing.Process(target=run_server).start()
+    multiprocessing.Process(target=kill_all_processes, args=(os.getpid(),)).start()
 
     # Run the app
     try:
@@ -77,6 +105,4 @@ if __name__ == '__main__':
         logger.exception(format_exc())
         raise
     finally:
-        process.terminate()
-        process.join()
         logger.info("Exited")
